ab285dc8aee521718e52c194ad4f6de163dd95e1
HowToKartoxa <anton@ozhigin.com>
2025-09-22 18:25:20 +0300

    Fix includes and CMakeLists.txt

--- lab2/Tests/Tests.cpp
+++ lab2/Tests/Tests.cpp
@@ -1,7 +1,7 @@
 #include <utils/array_algorithms.h>
 #include <utils/array_data.h>
 
-#include <catch2/catch_amalgamated.hpp>
+#include <catch2/catch_test_macros.hpp>
 
 #if defined(USE_WINAPI) or defined(USE_STD_THREAD)
 
